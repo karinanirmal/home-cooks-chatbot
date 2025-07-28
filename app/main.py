6e84e30a5ddf95e9afcff4f5bd902b28edd35c6a
karinanirmal <karinanirmal@gmail.com>
2025-07-28 17:04:37 -0400

    updated api

--- app/main.py
+++ app/main.py
@@ -1,7 +1,12 @@
 import streamlit as st
 from app.avatars import avatars
 import requests
+from dotenv import load_dotenv
 import os
+
+load_dotenv()
+openai.api_key=os.getenv("OPENAI_API_KEY")
+
 
 st.set_page_config(page_title= "Home Cooks Chatbot", page_icon="🍽️")
 st.title("Home Cooks Chatbot")
@@ -24,5 +29,15 @@ def call_openrouter(system_prompt,user_input):
         ]
     }
     response= requests.post("https://openrouter.ai/api/v1/chat/completions", headers=headers, json=body)
-    
-                   
+    if response.status_code==200:
+        return response.json()["choices"][0]["message"]["content"]
+    else:
+        return f"Error: {response.status_code}-{response.text}"
+if st.button("Ask the Avatar"):
+    if user_input.strip():
+        with st.spinner("Cooking up a response..."):
+            prompt= avatars[avatar_name]
+            answer=call_openrouter(prompt,user_input)
+            st.markdown(answer)
+    else:
+        st.warning("Please type what ingredients you have / want to eat.")
